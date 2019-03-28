eb6c15f0da8267e533fbac430f6265a59cf18fde
Naufal Muhammad <naufaluster@gmail.com>
2019-03-28 18:14:02 +0700

    Two Commit

--- src/main/java/com/practice/Database/CustomerData.java
+++ src/main/java/com/practice/Database/CustomerData.java
@@ -2,23 +2,44 @@ package com.practice.Database;
 
 
 import com.practice.Entity.Customer;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Owned by Naufal Muhammad Ischyros
  */
 
 public class CustomerData {
+
+    static CustomerData customerData;
+
     private static SessionFactory factory = new HibernateConfiguration().getSessionFactory();
 
-    private static Session sesn = factory.openSession();
-
     public Boolean addCustomer(Customer customer) {
-        try {
-            sesn.save(customer);
-        } finally {
+            Customer cust = new Customer();
 
+            try{
+                Session sesn = factory.openSession();
+                Query query = sesn.createQuery("FROM customer ORDER BY cif DESC");
+                query.setMaxResults(1);
+                Customer ocif = (Customer) query.uniqueResult();
+                String newCif = "";
+                if (ocif.getCif().isEmpty()) {
+                    newCif = "CIF00001";
+                } else {
+                    int lastLength = ocif.getCif().length();
+                    String tid = ocif.getCif().substring(5, lastLength);
+                    newCif = "CIF00" + String.format("%03d", Integer.parseInt(tid) + 1);
+                }
+                cust.setCif(newCif);
+            Transaction trx = sesn.beginTransaction();
+            sesn.save(customer);
+            sesn.flush();
+            trx.commit();
+        } catch (Exception e){
+            e.printStackTrace();
         } return true;
     }
 }
